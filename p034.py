8a4996fb6ce5f28d1a836b00f21b6d6dae464054
Thomas Cyron <thomas@thcyron.de>
2009-11-26 13:51:43 +0100

    Problem 034 solved, even if it's a bit slow

---
+++ p034.py
@@ -0,0 +1,10 @@
+from euler import factorial
+
+def p034(limit):
+    def iscurious(n):
+        return n == sum(map(factorial, map(int, (c for c in str(n)))))
+    return sum(filter(iscurious, xrange(10, limit)))
+
+if __name__ == "__main__":
+    from benchmark import benchmark
+    benchmark(p034, 999999)
